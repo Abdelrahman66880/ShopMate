bd58f86cd5725a5a5f9554dce8cc996451be9982
Abdelrahman66880 <abdofathi317@gmail.com>
2025-10-03 01:48:18 +0300

    handle the config file

--- shopmate/app.py
+++ shopmate/app.py
@@ -1,4 +1,3 @@
-from flask import Flask, render_template, request, jsonify
 import os
 from dotenv import load_dotenv
 
--- shopmate/config.py
+++ shopmate/config.py
@@ -1,1 +1,17 @@
 # config file
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+load_dotenv(r"/home/stranger/Desktop/ShopMate/.env")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+#paths
+DATA_PRODUCT_PATH = r"/home/stranger/Desktop/ShopMate/data/products.db"
+DATA_TEXT_PATH = r"/home/stranger/Desktop/ShopMate/data/policies.txt"
+STORE_DIRECTORY = r"/home/stranger/Desktop/ShopMate/data/processed"
+
+# Embeddings
+load_dotenv(r"/home/stranger/Desktop/ShopMate/.env")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+EMBEDDINGS = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
