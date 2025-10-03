dacb8a9b205ad832ba2079e70171f7bf858d35ae
Abdelrahman66880 <abdofathi317@gmail.com>
2025-10-03 04:31:02 +0300

    feat: add imports for ClassesEnums and LLMsModelsEnums in pretrained_router_model

---
+++ shopmate/routers/__init__.py
@@ -0,0 +1,2 @@
+from shopmate.routers.enums.ClassesEnums import ClassesEnums
+from shopmate.routers.enums.LLMsModelsEnums import LLMsModelsEnums
--- shopmate/routers/pretrained_router_model.py
+++ shopmate/routers/pretrained_router_model.py
@@ -1,1 +1,25 @@
 # pretrained router model file
+from .enums.ClassesEnums import ClassesEnums
+from .enums.LLMsModelsEnums import LLMsModelsEnums
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import torch.nn as nn
+
+class SemanticRouter:
+    def __init__(self):
+        model_name = LLMsModelsEnums.MODEL_NAME.value
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+    def predict(self, text):
+        inputs = self.tokenizer(text, return_tensors="pt", truncation=True, max_length=512, padding=True)
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+        probs = torch.nn.functional.softmax(outputs.logits, dim=-1)
+        product_prob = probs[0][1].item()
+        return ClassesEnums.PRODUCT_ROUTE_NAME.value if product_prob > 0.5 else ClassesEnums.CHITCHAT_ROUTE_NAME.value, product_prob
+
+    def guide(self, query: str) -> str:
+        result, confidence = self.predict(query)
+        return result
+
